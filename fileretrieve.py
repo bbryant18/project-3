c5e41416ef374a59adeef34949f9e6b3d9857e7d
bbryant18 <36276047+bbryant18@users.noreply.github.com>
2018-02-27 11:15:25 -0600

    Delete fileretrieve.py
    Since adam has this on his shared folder, we don't need it here

--- fileretrieve.py
+++
@@ -1,9 +0,0 @@
-# Borrowed from Adam Mikeal @ https://gist.github.com/amikeal
-
-from urllib.request import urlretrieve
-
-URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-LOCAL_FILE = 'local_copy.log'
-
-# Use urlretrieve() to fetch a remote copy and save into the local file path
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
